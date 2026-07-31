85954319f736f3eb767370428f833de66ee94576
vinay <vny.vk2@gmail.com>
2026-07-31 18:12:43 +0530

    fix(fs): export getAbortController from controlAbortControllers.ts
    - Added export function getAbortController to query active AbortController by path or ID.

--- src/main/fs/controlAbortControllers.ts
+++ src/main/fs/controlAbortControllers.ts
@@ -2,6 +2,10 @@ import logger from '../logger';
 
 const watcherInstances: { id: string; controller: AbortController }[] = [];
 
+export const getAbortController = (pathOrId?: string) => {
+  return watcherInstances.find((watcherInstance) => watcherInstance.id === pathOrId)?.controller;
+};
+
 export const closeAbortController = (pathOrId?: string) => {
   for (let i = 0; i < watcherInstances.length; i += 1) {
     const watcherInstance = watcherInstances[i];
